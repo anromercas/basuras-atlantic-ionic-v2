bd1f83a73550dac8793d6a1aa2b87b642b3e2f7a
Nuria-CORNER <anromercas@gmail.com>
2019-04-16 10:28:12 +0200

    cambio en IP de mi maquina en config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,2 +1,5 @@
+
 // export const URL_SERVICIOS = 'http://localhost:3000';
-export const URL_SERVICIOS = 'http://localhost:3000';
+
+// IP de mi maquina para poder usar el backend de node desde el movil en local
+export const URL_SERVICIOS = 'http://192.168.1.181:3000';
